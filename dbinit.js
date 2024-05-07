1901d475db2fb5076ba601f355171eb17b644c02
Reagan <83071617+ReaganS94@users.noreply.github.com>
2024-05-07 12:38:41 +0200

    Update dbinit.js

--- dbinit.js
+++ dbinit.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`MongoDB connected: ${conn.connection.host}`);
+  console.log(`MongoDB connected: ${conn.connection.name}`);
 };
 
 module.exports = connectDB;
